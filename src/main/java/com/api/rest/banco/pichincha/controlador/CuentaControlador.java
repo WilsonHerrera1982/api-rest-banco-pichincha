03c29120fa31449db8414fcf81a53d3a33510b26
WilsonHerrera1982 <43252591+WilsonHerrera1982@users.noreply.github.com>
2022-05-03 10:33:36 -0500

    Update CuentaControlador.java
    agrego guardar movimiento con saldo inicial

--- src/main/java/com/api/rest/banco/pichincha/controlador/CuentaControlador.java
+++ src/main/java/com/api/rest/banco/pichincha/controlador/CuentaControlador.java
@@ -1,5 +1,8 @@
 package com.api.rest.banco.pichincha.controlador;
 
+import java.util.Date;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.rest.banco.pichincha.dto.CuentaDto;
+import com.api.rest.banco.pichincha.entidad.Cuenta;
+import com.api.rest.banco.pichincha.entidad.Movimiento;
 import com.api.rest.banco.pichincha.servicio.CuentaServicio;
+import com.api.rest.banco.pichincha.servicio.MovimientoServicio;
 
 @RestController
 @RequestMapping("/api/cuenta")
@@ -17,11 +23,25 @@ public class CuentaControlador {
 
 	@Autowired
 	private CuentaServicio cuentaServicio;
+	@Autowired
+	private MovimientoServicio movimientoServicio;
+
 	@PostMapping("/guardar")
 	private ResponseEntity guardar(@RequestBody CuentaDto cuenta) {
-		
-		ResponseEntity responseEntity = cuentaServicio.guardar(cuenta);
+		Movimiento mov = new Movimiento();
+		Cuenta cuen = new Cuenta();
+		ResponseEntity responseEntity = new ResponseEntity<>(null);
+		UUID uuid = UUID.randomUUID();
 		try {
+			responseEntity = cuentaServicio.guardar(cuenta);
+			cuen = cuentaServicio.obtenerXId(cuenta.getNumeroCuenta());
+			mov.setMovimientoId(uuid);
+			mov.setCuenta(cuen);
+			mov.setFecha(new Date());
+			mov.setSaldo(cuenta.getSaldoInicial());
+			mov.setTipoMovimiento("Deposito");
+			mov.setValor(cuenta.getSaldoInicial());
+			movimientoServicio.guardarMovimiento(mov);
 
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
