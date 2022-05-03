2f8d7c6f58a2ae0af54157161a5a6e8a03c03f33
WilsonHerrera1982 <43252591+WilsonHerrera1982@users.noreply.github.com>
2022-05-03 10:31:28 -0500

    Update MovimientoServicio.java
    agrego metodo guardar movimiento entidad

--- src/main/java/com/api/rest/banco/pichincha/servicio/MovimientoServicio.java
+++ src/main/java/com/api/rest/banco/pichincha/servicio/MovimientoServicio.java
@@ -83,4 +83,8 @@ public class MovimientoServicio {
 
 		return movimientoRepositorio.reporte(identificacion, fechaInicio, fechaFin);
 	}
+	
+	public void guardarMovimiento(Movimiento mov) {
+		movimientoRepositorio.save(mov);
+	}
 }
