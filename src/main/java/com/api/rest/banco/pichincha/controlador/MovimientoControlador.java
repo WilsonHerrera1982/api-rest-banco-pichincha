e3af575c83b3a952c072bcfdb5a0c525e8092136
WilsonHerrera1982 <43252591+WilsonHerrera1982@users.noreply.github.com>
2022-07-29 13:25:27 -0500

    correcion errores

--- src/main/java/com/api/rest/banco/pichincha/controlador/MovimientoControlador.java
+++ src/main/java/com/api/rest/banco/pichincha/controlador/MovimientoControlador.java
@@ -1,97 +1,61 @@
-package com.api.rest.banco.pichincha.servicio;
+package com.api.rest.banco.pichincha.controlador;
 
-import java.math.BigDecimal;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.api.rest.banco.pichincha.dto.MovimientoDto;
-import com.api.rest.banco.pichincha.entidad.Cuenta;
+import com.api.rest.banco.pichincha.dto.ReporteDto;
 import com.api.rest.banco.pichincha.entidad.Movimiento;
-import com.api.rest.banco.pichincha.repositorio.CuentaRepositorio;
-import com.api.rest.banco.pichincha.repositorio.MovimientoRepositorio;
+import com.api.rest.banco.pichincha.servicio.MovimientoServicio;
 
-import ch.qos.logback.classic.Logger;
-
-@Service
-public class MovimientoServicio {
+@RestController
+@RequestMapping("/api/movimiento")
+public class MovimientoControlador {
 
 	@Autowired
-	private MovimientoRepositorio movimientoRepositorio;
-	@Autowired
-	private CuentaRepositorio cuentaRepositorio;
+	private MovimientoServicio movimientoServicio;
 
-	public ResponseEntity guardar(MovimientoDto movimiento) {
-		Optional<Cuenta> cuenta = cuentaRepositorio.findByNumeroCuenta(movimiento.getCuenta());
-		String mensaje = "";
-		List<Movimiento> listaMov = movimientoRepositorio.listaMovimientos(cuenta.get().getNumeroCuenta());
-		if(movimiento.getFecha() == null){
-			movimiento.setFecha(new Date());
-		}
-		if (listaMov.isEmpty()) {
-			if (!movimiento.getTipoMovimiento().equals("Retiro")) {
-				Movimiento movimiento2 = new Movimiento();
-				movimiento2.setFecha(movimiento.getFecha());
-				movimiento2.setMovimientoId(movimiento.getMovimientoId());
-				movimiento2.setTipoMovimiento(movimiento.getTipoMovimiento());
-				movimiento2.setCuenta(cuenta.get());
-				movimiento2.setSaldo(cuenta.get().getSaldoInicial());
-				movimiento2.setValor(movimiento.getValor());
-				movimientoRepositorio.save(movimiento2);
-				mensaje = "Registro guardardo con exito";
-			}else {				 
-				mensaje = "Saldo Insuficiente";
-			
-			}
-		} else {
-			List<Movimiento> lista = listaMov.stream().filter(mov -> mov.getFecha().equals(movimiento.getFecha()))
-					.collect(Collectors.toList());
-			List<Movimiento> ordenar = listaMov.stream().sorted(Comparator.comparing(Movimiento::getFecha).reversed())
-					.toList();
-			Movimiento movimiento2 = new Movimiento();
-			movimiento2.setCuenta(listaMov.get(0).getCuenta());
-			movimiento2.setFecha(movimiento.getFecha());
-			movimiento2.setMovimientoId(movimiento.getMovimientoId());
-			movimiento2.setTipoMovimiento(movimiento.getTipoMovimiento());
-			if (movimiento.getTipoMovimiento().equals("Retiro")) {
-				double suma = listaMov.stream().mapToDouble(m -> m.getValor().doubleValue()).sum();
-				if (movimiento.getValor().compareTo(listaMov.get(0).getCuenta().getLimiteDiario()) == 0 || 
-						BigDecimal.valueOf(suma).compareTo(listaMov.get(0).getCuenta().getLimiteDiario()) > 0) {
-					mensaje = "Limite diario de retiro exedido";
-					return ResponseEntity.ok(mensaje);
-				} else if (ordenar.get(0).getSaldo().compareTo(movimiento.getValor()) < 0) {
-					mensaje = "Saldo no disponible";
-					return ResponseEntity.ok(mensaje);
-				} else {
-					movimiento2.setValor(movimiento.getValor().negate());
-					movimiento2.setSaldo(ordenar.get(0).getSaldo().subtract(movimiento.getValor()));
-					movimientoRepositorio.save(movimiento2);
-					mensaje = "Registro guardardo con exito";
-				}
-			} else {
+	@PostMapping("/guardar")
+	private ResponseEntity guardar(@RequestBody MovimientoDto movimientoDto) {
 
-				movimiento2.setValor(movimiento.getValor());
-				movimiento2.setSaldo(ordenar.get(0).getSaldo().add(movimiento.getValor()));
-				movimientoRepositorio.save(movimiento2);
-				mensaje = "Registro guardardo con exito";
-			}
+		ResponseEntity responseEntity = movimientoServicio.guardar(movimientoDto);
+		try {
+
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
-		
-		return ResponseEntity.ok(mensaje);
+		return responseEntity;
+
 	}
 
-	public List<Movimiento> reporte(String identificacion, Date fechaInicio, Date fechaFin) {
-
-		return movimientoRepositorio.reporte(identificacion, fechaInicio, fechaFin);
-	}
-	
-	public void guardarMovimiento(Movimiento mov) {
-		movimientoRepositorio.save(mov);
+	@GetMapping("/reporte")
+	public List<ReporteDto> getReportIdentificacionAndBetween(@RequestParam String identificacion,
+			@RequestParam Date fechaInicio, @RequestParam Date fechaFin) {
+		List<Movimiento> lisMovimientos = new ArrayList<>();
+		List<ReporteDto> listaRetorno = new ArrayList<>();
+		lisMovimientos = movimientoServicio.reporte(identificacion, fechaInicio, fechaFin);
+		for (Movimiento mov : lisMovimientos) {
+			ReporteDto movi = new ReporteDto();
+			movi.setCuenta(mov.getCuenta().getNumeroCuenta());
+			movi.setFecha(mov.getFecha());
+			movi.setCliente(mov.getCuenta().getCliente().getPersona().getNombre());
+			movi.setMovimiento(mov.getValor().toString());
+			movi.setSaldo(mov.getSaldo().toString());
+			movi.setSaldoInicial(mov.getSaldo().toString());
+			movi.setTipo(mov.getTipoMovimiento());
+			listaRetorno.add(movi);
+		}
+		return listaRetorno;
 	}
 }
--- src/main/java/com/api/rest/banco/pichincha/controlador/PersonaControlador.java
+++ src/main/java/com/api/rest/banco/pichincha/controlador/PersonaControlador.java
@@ -1,41 +1,85 @@
-package com.api.rest.banco.pichincha.servicio;
+package com.api.rest.banco.pichincha.controlador;
 
-
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.api.rest.banco.pichincha.dto.PersonaDto;
 import com.api.rest.banco.pichincha.entidad.Cliente;
 import com.api.rest.banco.pichincha.entidad.Persona;
-import com.api.rest.banco.pichincha.repositorio.ClienteRepositorio;
-import com.api.rest.banco.pichincha.repositorio.PersonaRepositorio;
+import com.api.rest.banco.pichincha.servicio.ClienteServicio;
+import com.api.rest.banco.pichincha.servicio.PersonaServicio;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-@Service
-public class ClienteServicio {
+@RestController
+@RequestMapping("/api/cliente")
+public class PersonaControlador {
 
 	@Autowired
-	private ClienteRepositorio clienteRepositorio;
+	private PersonaServicio personaServicio;
 	@Autowired
-	private PersonaRepositorio personaRepositorio;
+	private ClienteServicio clienteServicio;
 
-	public ResponseEntity guardar(Cliente cliente) {
-		String mensaje = "";
-		Persona persona1 = personaRepositorio.findByIdentificacion(cliente.getPersona().getIdentificacion())
-				.orElse(cliente.getPersona());
-		if (persona1.getPersonaId() != null) {
-			cliente = new Cliente();
-
-			mensaje = "Cliente con esta identificacion ya existe!";
-
-		} else {
-			clienteRepositorio.save(cliente);
-			mensaje = "Registro almacenado correctamente";
-		}
+	@PostMapping("/guardar")
+	private ResponseEntity guardar(@RequestBody PersonaDto personaDto) {
 		
-		return ResponseEntity.ok(mensaje);
+		ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		try {
+			if(personaDto.getClave().equals("")|| personaDto.getClave() == null || personaDto.getIdentificacion() == null
+					|| personaDto.getNombre() == null) {
+				String mensaje = "Revisar los datos ingresados";
+				throw new Exception(mensaje);
+			}
+			Cliente cliente = new Cliente();
+			cliente.getPersona().setDireccion(personaDto.getDireccion());
+			cliente.getPersona().setEdad(Integer.valueOf(personaDto.getEdad()));
+			cliente.getPersona().setGenero(personaDto.getGenero());
+			cliente.getPersona().setIdentificacion(personaDto.getIdentificacion());
+			cliente.getPersona().setNombre(personaDto.getNombre());
+			cliente.getPersona().setTelefono(personaDto.getTelefono());
+			cliente.setClave(personaDto.getClave());
+			responseEntity = clienteServicio.guardar(cliente);
+		} catch (Exception e) {		
+			Logger.getLogger(PersonaControlador.class.getName()).log(Level.WARNING,null,e);			
+		}
+		return responseEntity;
 
 	}
 
-	
+	@GetMapping("/listar")
+	private ResponseEntity<List<Persona>> listarPersonas() {
+		return ResponseEntity.ok(personaServicio.listarPersonas());
+
+	}
+
+	@GetMapping(value = "(id)")
+	private ResponseEntity<Optional<Persona>> obtenerXId(@PathVariable("id") String id) {
+		return ResponseEntity.ok(personaServicio.obtenerXId(id));
+
+	}
+
+	@DeleteMapping("/eliminar")
+	private ResponseEntity<Persona> eliminar(@RequestBody String persona) {
+		personaServicio.eliminar(persona);
+
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/consultar")
+	private ResponseEntity<Optional<Persona>> consultar(@RequestParam String identificacion) {
+		return ResponseEntity.ok(personaServicio.obtenerXIdentificacion(identificacion));
+
+	}
 }
